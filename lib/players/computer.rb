110d64d7a73b1f2c8a425f8e4956988c6e5cc957
surpreet.mundra <surpreet.mundra@gmail.com>
2018-09-17 17:12:34 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -17,7 +17,7 @@ module Players
         move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
 
       # If you went second (and took the middle) and the other player has occupied opposing corner squares, blow up the attempted trap by taking a side square.
-      elsif board.position(1) == board.position(9) || board.position(3) == board.position(7)
+    elsif board.taken?(1) && board.position(1) == board.position(9) || board.taken?(3) && board.position(3) == board.position(7)
         binding.pry
         move = "2"
 

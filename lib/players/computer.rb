f3ebf784f1ff51ac681dc9468ae528d6bef2ae05
surpreet.mundra <surpreet.mundra@gmail.com>
2018-09-17 18:26:05 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,7 +3,7 @@ module Players
   class Computer < Player
 
     def check_win_combos
-      
+
       Game::WIN_COMBINATIONS.each do |combo|
         move = nil
         # First, check whether you have any chances to win, since it doesn't matter whether the opponent has a chance to win if you can win first.
@@ -17,27 +17,25 @@ module Players
           binding.pry
         end
       end
-      move 
-    end 
-    
+      move
+    end
+
     def move(board)
-      
-      check_win_combos = 
 
       # When going first, take the middle square. When going second, take the middle square if it isn't yet taken.
-      if !board.taken?(5)
+      if check_win_combos == nil && !board.taken?(5)
         move = "5"
 
       # If going second and the middle square is taken, take the upper-left corner square.
-      elsif board.turn_count == 1
+      elsif check_win_combos == nil && board.turn_count == 1
         move = "1"
 
       # If you went first (and took the middle), take a corner square with your second move.
-      elsif board.turn_count == 2
+      elsif check_win_combos == nil && board.turn_count == 2
         move = [1, 3, 7, 9].detect{|i| !board.taken?(i)}.to_s
 
       # If you went second (and took the middle) and the other player has occupied opposing corner squares, blow up the attempted trap by taking a side square.
-      elsif board.turn_count == 3 && board.taken?(1) && board.position(1) == board.position(9) || board.turn_count == 3 && board.taken?(3) && board.position(3) == board.position(7)
+      elsif check_win_combos == nil && board.turn_count == 3 && board.taken?(1) && board.position(1) == board.position(9) || board.turn_count == 3 && board.taken?(3) && board.position(3) == board.position(7)
         move = "2"
 
         # If none of the WIN_COMBINATIONS patterns have two squares taken by the same token and a third empty square, play into the first open square you find, first checking corners and then checking sides.
